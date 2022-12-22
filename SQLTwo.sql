c650cc58ea0909a129574fd8c3a7de6fe003d53c
Ruchi Sharma <statsnotes21@gmail.com>
2022-12-22 15:52:59 -0600

    Update SQLTwo.sql

--- SQLTwo.sql
+++ SQLTwo.sql
@@ -7,7 +7,6 @@
 -----------
 
 -- 1699. Number of Calls Between Two Persons
-
 SELECT person1, person2, COUNT(*) as call_count, SUM(duration) as total_duration FROM
 (
     SELECT
@@ -20,19 +19,16 @@ SELECT person1, person2, COUNT(*) as call_count, SUM(duration) as total_duration
 GROUP BY person1, person2
 
 -- 1251. Average Selling Price 
-
 SELECT P.product_id, ROUND(SUM(units*price)/SUM(units), 2) average_price 
 FROM Prices P JOIN UnitsSold U ON P.product_id = U.product_id 
 AND purchase_date BETWEEN start_date and end_date
 GROUP BY P.product_id
 
 -- 1571. Warehouse Manager 
-
 SELECT W.name AS warehouse_name, SUM(P.width * p.length * p.height * w.units) AS VOLUME 
 FROM Warehouse W INNER JOIN Products P ON W.product_id = P.product_id GROUP BY W.name
 
 -- 1445. Apples & Oranges
-
 SELECT A.sale_date, A.S1 - B.S2 AS diff
 FROM (SELECT sale_date, SUM(sold_num) AS S1 FROM Sales WHERE fruit = 'apples' GROUP BY sale_date) A
 INNER JOIN (SELECT sale_date, SUM(sold_num) AS S2 FROM Sales WHERE fruit = 'oranges' GROUP BY sale_date) B
@@ -43,7 +39,6 @@ ON A.sale_date = B.sale_date
 -----------
 
 -- 1193. Monthly Transactions I
-
 SELECT date_format(trans_date,"%Y-%m") AS month, 
        country, 
        COUNT(id) AS trans_count, 
@@ -54,17 +49,14 @@ FROM Transactions
 GROUP BY month, country 
 
 -- 1633. Percentage of Users Attended a Contest
-
 SELECT contest_id, ROUND(100*COUNT(user_id)/(SELECT COUNT(DISTINCT user_id) FROM Users), 2) AS percentage
 FROM Register GROUP BY contest_id ORDER BY percentage DESC, contest_id
 
 -- 1173. Immediate Food Delivery I
-
 SELECT ROUND(100*SUM(CASE WHEN customer_pref_delivery_date = order_date THEN 1 ELSE 0 END)/(SELECT COUNT(delivery_id) FROM Delivery), 2)
 AS immediate_percentage FROM Delivery 
 
 -- 1211. Queries Quality and Percentage
-
 SELECT query_name,
        ROUND(AVG(rating/position), 2) AS quality, 
        ROUND(100*SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END)/COUNT(query_name), 2) AS poor_query_percentage
