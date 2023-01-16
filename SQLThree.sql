4702c14c7a3d9a81dcb158b25690a70b50b6db58
Ruchi Sharma <statsnotes21@gmail.com>
2023-01-16 17:05:06 -0600

    Update SQLThree.sql

--- SQLThree.sql
+++ SQLThree.sql
@@ -1,6 +1,6 @@
 -- Solution Archive for LeetCode SQL Study Plan - III
 -- 10 Days of SQL practice, created by Ruchi Sharma [UT Austin '23, IIT Roorkee '21]
--- [30 Dec 2022, 8 Jan 2023] 
+-- [16 Jan 2023, 25 Jan 2023] 
 
 -----------
 -- Day 1 --
