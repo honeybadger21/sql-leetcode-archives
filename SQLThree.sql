0c1c6150015fde01f17c0cda79aab2994069b904
Ruchi Sharma <statsnotes21@gmail.com>
2023-01-23 01:28:08 -0600

    Update SQLThree.sql

--- SQLThree.sql
+++ SQLThree.sql
@@ -119,6 +119,16 @@ GROUP BY user_id ORDER BY user_id
 -----------
 
 -- 1949. Strong Friendship
+WITH CTE AS(SELECT user1_id, user2_id FROM friendship 
+            UNION 
+            SELECT user2_id, user1_id FROM friendship)
+
+SELECT C1.user1_id, C2.user1_id AS user2_id, COUNT(*) AS common_friend
+FROM CTE C1 JOIN CTE C2 ON C1.user1_id < C2.user2_id AND C1.user2_id = C2.user1_id
+WHERE (C1.user1_id, C2.user1_id) IN (SELECT * FROM friendship)
+GROUP BY 1, 2
+HAVING COUNT(*) >= 3
+
 -- 1532. The Most Recent Three Orders
 -- 1126. Active Businesses
 -- 1831. Maximum Transaction Each Day
