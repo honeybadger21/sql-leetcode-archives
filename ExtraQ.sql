ff8ca8877aaced0f2ec5ccfdb71e94c4863ab236
Ruchi Sharma <statsnotes21@gmail.com>
2024-06-04 00:14:54 -0500

    Update ExtraQ.sql

--- ExtraQ.sql
+++ ExtraQ.sql
@@ -19,3 +19,11 @@ group by 1
 select name, bonus from employee a left join bonus b on a.empId = b.empId 
 where bonus is null or bonus < 1000
 
+-- 1280. Students and Examinations
+-- good cross join use 
+SELECT s.student_id, s.student_name, sub.subject_name, COUNT(e.subject_name) AS attended_exams
+FROM Students s
+CROSS JOIN Subjects sub
+LEFT JOIN Examinations e ON s.student_id = e.student_id AND sub.subject_name = e.subject_name
+GROUP BY s.student_id, s.student_name, sub.subject_name
+ORDER BY s.student_id, sub.subject_name;
