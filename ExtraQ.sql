de8c9eb0bd6bd1af76a79cb2473127f9b5a96748
Ruchi Sharma <statsnotes21@gmail.com>
2024-06-03 21:15:30 -0500

    Update ExtraQ.sql

--- ExtraQ.sql
+++ ExtraQ.sql
@@ -1,2 +1,5 @@
 -- 1683. Invalid Tweets
 select distinct tweet_id from tweets where length(content) > 15
+
+-- 1378. Replace employee ID with the unique identifier 
+select unique_id, name from employees e1 left join employeeUNI e2 on e1.id=e2.id
