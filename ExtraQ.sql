693eabe6e4bdef8d98cf95b6a72c346c9c120001
Ruchi Sharma <statsnotes21@gmail.com>
2024-06-03 21:58:19 -0500

    Update ExtraQ.sql

--- ExtraQ.sql
+++ ExtraQ.sql
@@ -6,3 +6,12 @@ select unique_id, name from employees e1 left join employeeUNI e2 on e1.id=e2.id
 
 -- 1068. Product Sales Analysis I
 select product_name, year, price from sales a left join product b on a.product_id = b.product_id
+
+-- 1661. Average Time of Process per Machine
+select machine_id, round(sum(ok)/count(process_id), 3) as processing_time from (
+select * from(
+select a1.machine_id, a1.process_id, round((a2.timestamp-a1.timestamp), 3) as ok from activity a1 join activity a2
+on a1.machine_id = a2.machine_id and a1.process_id = a2.process_id) t where ok>0
+) t2
+group by 1
+
