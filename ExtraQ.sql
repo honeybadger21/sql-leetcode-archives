eb706d437f038edaed248298359d66e7e7ba0875
Ruchi Sharma <statsnotes21@gmail.com>
2024-06-03 21:21:03 -0500

    Update ExtraQ.sql

--- ExtraQ.sql
+++ ExtraQ.sql
@@ -3,3 +3,6 @@ select distinct tweet_id from tweets where length(content) > 15
 
 -- 1378. Replace employee ID with the unique identifier 
 select unique_id, name from employees e1 left join employeeUNI e2 on e1.id=e2.id
+
+-- 1068. Product Sales Analysis I
+select product_name, year, price from sales a left join product b on a.product_id = b.product_id
